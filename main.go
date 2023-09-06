eb0c0172f364260d42da29bf299b097358a01d9e
Siddharth Kundu <sk31395@gmail.com>
2023-09-06 02:01:08 -0400

    initial changes

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+	"os"
+)
+
+const PORT = ":8080"
+
+func pageHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	pageID := vars["id"]
+	fileName := "files/" + pageID + ".html"
+	_, err := os.Stat(fileName)
+	if err != nil {
+		fileName = "files/404.html"
+	}
+
+	http.ServeFile(w, r, fileName)
+}
+
+func main() {
+	rtr := mux.NewRouter()
+	rtr.HandleFunc("/pages/{id:[0-9]+}", pageHandler)
+	http.Handle("/", rtr)
+	http.ListenAndServe(PORT, nil)
+}
